b64314cb8eec4099bf5cf1757de2e71278f6bee0
matiah-hub <mathiaz16.mc@gmail.com>
2026-03-05 00:28:58 -0300

    Delete clientesv2 directory

--- clientesv2/src/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const poolString = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-});
-
-module.exports = { poolString };
